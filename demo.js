52b7d1b38825babfaaf08739fd99e99ded073e74
AnantG-hub <anantgairola5@gmail.com>
2025-06-20 14:36:38 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,7 @@
+<html>
+    <head>my doc</head>
+    <body>
+        <button type ="button" onclick="document.getElementById('demo').innerHTML=Date()">click</button>
+        <p id="demo"></p>
+    </body>
+</html>
