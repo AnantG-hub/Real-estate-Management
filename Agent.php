4c8ca781b44dd3b9b795315d52479b74b3869411
AnantG-hub <anantgairola5@gmail.com>
2025-06-20 14:28:01 +0530

    added Agent.php

---
+++ Agent.php
@@ -0,0 +1,104 @@
+<html>
+    <head>
+        <title>Agent</title>
+        <link rel="stylesheet" type="text/css" href="nav5.css">
+        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+            h1
+            {
+                font-family:'Courier New', Courier, monospace;
+                font: 5em sans-serif;
+                text-align: center;
+                background-color:rgb(rgb(200, 0, 255), rgb(0, 68, 128), rgb(255, 170, 0)) ;
+                font-variant:small-caps;
+                border: 1rem solid #666;
+                box-shadow:1 5rem;
+                background: #999;
+            }
+            .clr{
+                color: brown;
+            }
+        </style>
+    </head>
+    <body>
+        <section class="Agent" id="Agent">
+            <h1 class="heading">Commercial<span class="clr">Agents</span></h1>
+            <?php include "header.php" ?>
+            <div class="boxx">
+                <div class="box">
+                    <a href="contact.php"class="fa fa-envelope"></a>
+                    <a href="contact.html "class="fa fa-phone"></a>
+                    <img src="agent1.png" alt="">
+                    <h2><u>Realtor Bios</u></h2>
+                    <span>Agent:-</span>
+                    <h3><i class="fa fa-quote-left"></i> Awesome Services! <i class="fa fa-quote-right"></i></h3>
+                    <p>The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+                    <div class="app-icon">
+                        <a href="#" class="fa fa-facebook-f"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-whatsapp"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                        <a href="#" class="fa fa-telegram"></a>
+                    </div>
+                </div>
+                <div class="box">
+                    <a href="contact.php"class="fa fa-envelope"></a>
+                    <a href="contact.html "class="fa fa-phone"></a>
+                    <img src="agent2.png" alt="">
+                    <h2><u>John Wills</u></h2>
+                    <span>Agent:-</span>
+                    <h3><i class="fa fa-quote-left"></i> Great & Talented Team! <i class="fa fa-quote-right"></i></h3>
+                    <p>They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+                    <div class="app-icon">
+                        <a href="#" class="fa fa-facebook-f"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-whatsapp"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                        <a href="#" class="fa fa-telegram"></a>
+                    </div>
+                </div>
+                <div class="box">
+                    <a href="contact.php"class="fa fa-envelope"></a>
+                    <a href="contact.html"class="fa fa-phone"></a>
+                    <img src="agent3.png" alt="">
+                    <h2><u>Emily Willim</u></h2>
+                    <span>Agent:-</span>
+                    <h3><i class="fa fa-quote-left"></i> Wonderful Support! <i class="fa fa-quote-right"></i></h3>
+                    <p>The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure.</p>
+                    <div class="app-icon">
+                        <a href="#" class="fa fa-facebook global-radius"></a>
+                        <a href="#" class="fa fa-twitter global-radius"></a>
+                        <a href="#" class="fa fa-whatsapp global-radius"></a>
+                        <a href="#" class="fa fa-instagram global-radius"></a>
+                        <a href="#" class="fa fa-linkedin global-radius"></a>
+                        <a href="#" class="fa fa-telegram global-radius"></a>
+                    </div>
+                </div>
+                <div class="box">
+                    <a href="contact.php"class="fa fa-envelope"></a>
+                    <a href="contact.html "class="fa fa-phone"></a>
+                    <img src="agent4.jpg" alt="">
+                    <h2><u>Rose Denze</u></h2>
+                    <span>Agent:-</span>
+                    <h3><i class="fa fa-quote-left"></i> Awesome Services! <i class="fa fa-quote-right"></i></h3>
+                    <p>Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+                    <div class="app-icon">
+                        <a href="#" class="fa fa-facebook-f"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-whatsapp"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                        <a href="#" class="fa fa-telegram"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br>
+        <section class="footer">
+        <?php include "footer.php" ?>
+        </section>
+    </body>
+</html>
